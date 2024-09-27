7c9b7695e89f9644dbbfe4aefe84a6f5178c6086
Davy Ribeiro <davy.oliv.ribeiro@gmail>
2024-09-26 22:22:27 -0300

    Categoria e Card

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NavBarComponent } from '../nav-bar/nav-bar.component';
+import { CategoryBarComponent } from '../category-bar/category-bar.component';
+import { CourseComponent } from '../course/course.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeaderComponent,NavBarComponent],
+  imports: [HeaderComponent,NavBarComponent,CategoryBarComponent,CourseComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
---
+++ src/app/models/couses.spec.ts
@@ -0,0 +1,7 @@
+import { Couses } from './couses';
+
+describe('Couses', () => {
+  it('should create an instance', () => {
+    expect(new Couses()).toBeTruthy();
+  });
+});
